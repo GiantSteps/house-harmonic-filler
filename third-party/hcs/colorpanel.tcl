3c8c92ceac93140cbc655eb21e72b542064b12fc
Ángel Faraldo <angelfaraldo@gmail.com>
2016-05-23 14:29:16 +0200

    dependencies OSX

--- third-party/hcs/colorpanel.tcl
+++
@@ -1,8 +0,0 @@
-
-namespace eval ::hcs::colorpanel:: {
-}
-
-proc ::hcs::colorpanel::open {objectid initialcolor} {
-    set color [tk_chooseColor -initialcolor $initialcolor]
-    pdsend "$objectid callback $color"
-}
